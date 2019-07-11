4de85b8e0bdd8df32095432371ed0c9035749428
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 06:41:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 
 def monopoly_with_third_tier
 	array = monopoly_with_second_tier
-	array[:railroads][:rent_in_dollars] = {
+	array[:railroads][:rent_in_dollars] => {
 	  :one_piece_owned => 25, 
 	two_pieces_owned: 50, 
 	three_pieces_owned: 100, 
