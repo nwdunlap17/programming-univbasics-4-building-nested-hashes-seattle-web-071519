d584e0ebd4ee98f67e0beb84d1075606a93a146c
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 06:30:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def monopoly
 	monopoly = {}
+  monopoly[:railroads] = {}
+  return monopoly
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {}
-  monopoly[:railroads] = {}
-  return monopoly
 end
 
 def monopoly_with_third_tier
